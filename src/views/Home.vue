7aaaaca047ef793b2a74710e53bd2e78501450e6
richarne <richarne@uio.no>
2020-08-06 21:32:02 +0200

    Start frontend

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <agile ref="main" :fade="true">...</agile>
+    <agile
+      ref="thumbnails"
+      :as-nav-for="[$refs.main]"
+      :slides-to-show="4"
+      autoplay
+      >...</agile
+    >
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { VueAgile } from 'vue-agile'
+
+export default {
+  name: 'Home',
+  components: {
+    agile: VueAgile,
+  },
+}
+</script>
